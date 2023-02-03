e6fdae4c090041ca76b3067f3ff86efe601c85a0
chyhu <ch3384@nyu.edu>
2023-02-03 10:51:27 -0500

    Modify `DataManipulator` to match table schema

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,26 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    // calculate and set values to be returned to table
+    const [price_abc, price_def] = [...serverResponds.map(res => (res.top_ask.price + res.top_bid.price) / 2)];
+    const ratio = price_abc / price_def;
+    const timestamp = serverResponds[0].timestamp > serverResponds[1].timestamp ?
+      serverResponds[0].timestamp : serverResponds[1].timestamp;
+    const upper_bound = 1 + 0.05;
+    const lower_bound = 1 - 0.05;
+    const trigger_alert = (ratio > upper_bound || ratio < lower_bound) ? ratio : undefined;
+    return { price_abc, price_def, ratio, timestamp, upper_bound, lower_bound, trigger_alert }
   }
 }
